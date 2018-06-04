fb83dcd4f1a5b850c63923ad8aebdb169ebd57ed
Lindsay Power-Harding <lindsph7@gmail.com>
2018-06-03 22:13:21 -0400

    contact form, styling etc

--- scripts/script.js
+++ scripts/script.js
@@ -22,4 +22,48 @@ $(function() {
             $('.navigation').addClass("stickyHeader");
         }
     });
+
+    new TypeIt('.type-it', {
+        // strings: ["Web Developer", "Solving Problems, Developing Solutions"],
+        speed: 75,
+        autoStart: false
+    })
+        .type("Web Developer")
+        .pause(2000)
+        .delete()
+        .type("Solving Problems, Developing Solutions")
+        .pause(1000)
+    // END OF TYPE-IT FUNCTION
+
+    let $input;
+
+    function onInputFocus(event) {
+        const $target = $(event.target);
+        const $parent = $target.parent();
+        $parent.addClass('input--filled');
+    };
+
+    function onInputBlur(event) {
+        const $target = $(event.target);
+        const $parent = $target.parent();
+
+        if (event.target.value.trim() === '') {
+            $parent.removeClass('input--filled');
+        }
+    };
+
+    $(document).ready(function () {
+        $input = $('.input__field');
+
+        // in case there is any value already
+        $input.each(function () {
+            if ($input.val().trim() !== '') {
+                const $parent = $input.parent();
+                $parent.addClass('input--filled');
+            }
+        });
+
+        $input.on('focus', onInputFocus);
+        $input.on('blur', onInputBlur);
+    });
 });
